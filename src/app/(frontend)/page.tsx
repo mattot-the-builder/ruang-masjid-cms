f895e938a93f9b3df6e4ea940311f00238eaf3ce
mattot <mattot.builder@gmail.com>
2025-09-27 02:15:28 +0800

    feat: added dummy Hero

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,16 +4,20 @@ import './styles.css'
 import getAuthenticatedUser from '@/helpers/auth/get-authenticated-user'
 import { Button } from '@/components/ui/button';
 
+function Hero() {
+    return <div className="w-full h-[36rem] bg-red-100"></div>
+}
+
 export default async function HomePage() {
 
-  const user = await getAuthenticatedUser();
+    const user = await getAuthenticatedUser();
 
-
-  return (
-    <div className="home">
-      <Button>test button</Button>
-      test
-      {user?.email}
-    </div>
-  )
+    return (
+        <div className="home">
+            <Hero />
+            <Button>test button</Button>
+            test
+            {user?.email}
+        </div>
+    )
 }
