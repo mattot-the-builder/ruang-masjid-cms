8b6817655bc1c8f07e3ef14db5e1439b9b75c1ba
mattot <mattot.builder@gmail.com>
2025-09-27 23:47:46 +0800

    styles: added typography component from shadcn

---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,58 @@
+import { cn } from "@/lib/utils"
+
+type TypographyProps = {
+    children: React.ReactNode,
+    className?: string
+}
+
+export function H1({ children, className }: TypographyProps) {
+    return (
+        <h1 className={cn("scroll-m-20 text-4xl font-extrabold tracking-tight text-balance", className)}>
+            {children}
+        </h1>
+    )
+}
+
+export function H2({ children, className }: TypographyProps) {
+    return (
+        <h2 className={cn("scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0", className)}>
+            {children}
+        </h2>
+    )
+}
+
+export function H3({ children, className }: TypographyProps) {
+    return (
+        <h3 className={cn("scroll-m-20 text-2xl font-semibold tracking-tight", className)}>
+            {children}
+        </h3>
+    )
+}
+
+export function H4({ children, className }: TypographyProps) {
+    return (
+        <h4 className={cn("scroll-m-20 text-xl font-semibold tracking-tight", className)}>
+            {children}
+        </h4>
+    )
+}
+
+export function P({ children, className }: TypographyProps) {
+    return (
+        <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}>
+            {children}
+        </p>
+    )
+}
+
+export function Large({ children, className }: TypographyProps) {
+    return <div className={cn("text-lg font-semibold", className)}>{children}</div>
+}
+
+export function List({ children, className }: TypographyProps) {
+    return (
+        <ul className={cn("my-6 ml-6 list-disc [&>li]:mt-2", className)}>
+            {children}
+        </ul>
+    )
+}
